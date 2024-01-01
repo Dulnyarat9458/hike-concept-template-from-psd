8cc0e6f9e8bce2254779beb90058725f902d9ea1
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2024-01-01 13:39:10 +0700

    create complex components

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,50 @@
+accentColor = "#ffae47";
+accentSecondaryColor = "#ff2e17";
+primaryColor = "#2c2e35";
+secondaryColor = "#ffffff";
+tertiaryColor = "#e0e0e0";
+
+window.addEventListener('scroll', function () {
+    const scrollX = window.scrollY || document.documentElement.scrollTop;
+    const screenWidth = window.innerWidth;
+
+    if (scrollX > 100 && screenWidth >= 992) {
+        document.getElementById("nav-panel").style.backgroundColor = primaryColor;
+        if (scrollX > 300) {
+            document.getElementById("nav-panel-primary").style.height = "0px";
+        } else {
+            document.getElementById("nav-panel-primary").style.height = "66px";;
+        }
+    }
+    else if (scrollX > 100 && screenWidth < 992) {
+        document.getElementById("nav-panel").style.backgroundColor = primaryColor;
+    } else {
+        document.getElementById("nav-panel").style.backgroundColor = "transparent";
+        document.getElementById("nav-panel-primary").style.backgroundColor = "transparent";
+        document.getElementById("nav-panel-primary").style.height = "66px"
+    }
+
+});
+
 const closeSidebar = () => {
     document.getElementById("sidebar").style.width = "0px";
-    document.getElementById("sidebar").style.display = "none";
     document.getElementById("search-bar").style.display = "none";
     document.getElementById("backdrop-panel").style.display = "none";
-
 }
 
 const openSidebar = () => {
     document.getElementById("sidebar").style.width = "100% ";
-    document.getElementById("sidebar").style.display = "block";
     document.getElementById("backdrop-panel").style.display = "block";
 }
 
 
 const closeSearch = () => {
-    document.getElementById("search-bar").style.width = "0px";
-    document.getElementById("search-bar").style.display = "none";
-    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("search-bar").style.height = "0px";
+    document.getElementById("sidebar").style.width = "0px";
     document.getElementById("backdrop-panel").style.display = "none";
-
 }
 
 const openSearch = () => {
-    document.getElementById("search-bar").style.width = "100% ";
-    document.getElementById("search-bar").style.display = "block";
+    document.getElementById("search-bar").style.height = "100%";
     document.getElementById("backdrop-panel").style.display = "block";
 }
